48f52113523edbb745e4377e1fa30dc1e43e2475
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-02-19 12:11:34 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -6,7 +6,7 @@ let fetch = require('node-fetch')
 let moment = require('moment-timezone')
 const defaultMenu = {
   before: `
-┌─〔 R-Txzy 〕
+┌─〔 Hanii 〕
 ├ Hai, *%name!*
 ├ Memory Used : *${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
 └────
@@ -187,9 +187,9 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       return conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
         "listMessage": {
           "title": `${ucapan()}, ${name}`.trim(),
-          "description": "*© R-Txzy*",
+          "description": "*© Hanicans:v*",
           "footerText": "Jika menemukan bug, error atau kesulitan dalam penggunaan silahkan laporkan/tanyakan kepada owner.",
-          "buttonText": "Click Here",
+          "buttonText": "Klik disini Kak >_<",
           "listType": "SINGLE_SELECT",
           "sections": [
             {
